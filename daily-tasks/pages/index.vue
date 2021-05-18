f6651c6c657f2ca47bef8cc3ad848de59ffb149e
Sinje Kühme <sinje.kuehme@gmail.com>
2021-05-18 14:50:21 +0200

    wip

--- daily-tasks/pages/index.vue
+++ daily-tasks/pages/index.vue
@@ -1,75 +1,49 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        daily-tasks
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="title">A simple app to prioritize your daily tasks</div>
+    <div class="description">
+      Enter a task and decide whether it is urgent and important, important but
+      not urgent, urgent but not important, or neither urgent nor important.
     </div>
+    <!-- turn this into components -->
+    <div class="container__enter-task"></div>
+    <div class="container__list-tasks"></div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
+  background-color: aliceblue;
+  margin: 40px 30px;
   min-height: 100vh;
-  display: flex;
+  display: block;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Helvetica Neue", Arial, sans-serif;
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
+  font-weight: 100;
+  font-size: 50px;
+  color: #04182d;
   letter-spacing: 1px;
+  padding: 20px 0;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
+.description {
+  font-family: "Helvetica Neue", Arial, sans-serif;
+  font-weight: 250;
+  font-size: 20px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
---
+++ daily-tasks/translations/index.js
@@ -0,0 +1,5 @@
+export default {
+  title: "A simple app to prioritize your daily tasks",
+  introduction:
+    "Enter a task and decide whether it is urgent and important, important but not urgent, urgent but not important, or neither urgent nor important"
+};
